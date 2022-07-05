81119094396098c5286f64fc28ae1911471d69fd
unknown <mnonelelinquma91@gmail.com>
2022-07-05 09:30:06 +0200

    add date

--- script.js
+++ script.js
@@ -4,3 +4,4 @@ document.addEventListener('DOMContentLoaded', function () {
 
 const form = document.querySelector('.add-form')
 const tBody = document.querySelector('tbody')
+const date = document.getElementById('date')
