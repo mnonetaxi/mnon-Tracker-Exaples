07eaa79663ad8e92888115c4b9cc284d7b0c268b
unknown <mnonelelinquma91@gmail.com>
2022-07-05 09:42:12 +0200

    add income.expense

--- script.js
+++ script.js
@@ -5,5 +5,5 @@ document.addEventListener('DOMContentLoaded', function () {
 const form = document.querySelector('.add-form')
 const tBody = document.querySelector('tbody')
 const date = document.getElementById('date')
-const description = document.getElementById('description')
-const 
+const expense = document.getElementById('expense')
+const income = document.getElementById('income')
